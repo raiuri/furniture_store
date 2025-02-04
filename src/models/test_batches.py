18321c384a077ced01a391db22db15fb3900fbea
Raiuri Santos <raiuresantos@ymail.com>
2025-02-04 17:35:17 -0300

    add test_allocating_to_a_batch_reduces_the_avaliable_quantity fuction

---
+++ src/models/test_batches.py
@@ -0,0 +1,18 @@
+# Order Line
+  # order reference
+  # Sku
+  # quantity
+
+# Batches of stock
+  # reference
+  # sku
+  # quantity
+
+
+import pytest
+
+
+def test_allocating_to_a_batch_reduces_the_avaliable_quantity():
+  pytest.fail("todo")
+
+
