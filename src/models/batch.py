35610c94f11d2f57c1ca5ee03d5d034a8cb9b781
Raiuri Santos <raiuresantos@ymail.com>
2025-02-04 18:51:47 -0300

    make test_allocating_to_a_batch_reduces_the_avaliable_quantity pass

--- src/models/batch.py
+++ src/models/batch.py
@@ -2,38 +2,30 @@ from dataclasses import dataclass
 from typing import Optional
 from datetime import date
 
+# Order Line
+  # order reference
+  # sku
+  # quantity
+
+# Batches of stock
+  # reference
+  # sku
+  # quantity
+
 @dataclass(frozen=True)
 class OrderLine:
-  orderid: str
+  order_id: str
   sku: str
-  qty: int
+  quantity: int
 
 
 class Batch:
-    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
-        self.reference = ref
-        self.sku = sku
-        self.eta = eta
-        self._purchased_quantity = qty
-        self._allocations = set()  # type: Set[OrderLine]
+  def __init__(self, ref: str, sku: str, quantity: int):
+    self.ref = ref
+    self.sku = sku
+    self.avaliable_qty = quantity
 
-    def allocate(self, line: OrderLine):
-        if self.can_allocate(line):
-            self._allocations.add(line)
 
-    def deallocate(self, line: OrderLine):
-        if line in self._allocations:
-            self._allocations.remove(line)
-
-    @property
-    def allocated_quantity(self) -> int:
-        return sum(line.qty for line in self._allocations)
-
-    @property
-    def available_quantity(self) -> int:
-        return self._purchased_quantity - self.allocated_quantity
-
-    def can_allocate(self, line: OrderLine) -> bool:
-        return self.sku == line.sku and self.available_quantity >= line.qty
-    
+  def allocate(self, order_line: OrderLine):
+    self.avaliable_qty -= order_line.quantity
 
--- src/models/test_batches.py
+++ src/models/test_batches.py
@@ -8,11 +8,14 @@
   # sku
   # quantity
 
-
-import pytest
-
+from .batch import OrderLine, Batch
 
 def test_allocating_to_a_batch_reduces_the_avaliable_quantity():
-  pytest.fail("todo")
+  order_line = OrderLine(sku='abc', order_id='123', quantity=3)
+  batch = Batch(ref=333, sku='abc', quantity=20)
+
+  batch.allocate(order_line)
+
+  assert batch.avaliable_qty == 17
 
 
