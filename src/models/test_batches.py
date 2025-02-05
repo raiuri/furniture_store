eef72d025dcf3539edae4486fb3f1b030e3dfa5c
Raiuri Santos <raiuresantos@ymail.com>
2025-02-05 12:40:18 -0300

    refactor test_batches

--- src/models/test_batches.py
+++ src/models/test_batches.py
@@ -8,11 +8,20 @@
   # sku
   # quantity
 
+# The name of our unity test describes the behavior that we wanto to see from the sysyem
+# and the names of the classes and variables that we use are taken from the business jargon.
+
 from .batch import OrderLine, Batch
 
+def make_batch_and_line(sku, batch_qty, line_qty):
+  return (
+    Batch(ref=333, sku=sku, quantity=batch_qty),
+    OrderLine(sku=sku, order_id='123', quantity=line_qty)
+  )
+
+
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-  order_line = OrderLine(sku='BLACK_CHAIR', order_id='123', quantity=3)
-  batch = Batch(ref=333, sku='BLACK_CHAIR', quantity=20)
+  batch, order_line = make_batch_and_line(sku='BLACK_CHAIR', batch_qty=20, line_qty=3)
 
   batch.allocate(order_line)
 
@@ -20,21 +29,22 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
 
 
 def test_can_allocate_if_available_greater_than_required():
-  order_line = OrderLine(sku='BLACK_CHAIR', order_id='123', quantity=5)
-  batch = Batch(ref=333, sku='BLACK_CHAIR', quantity=10)
+  batch, order_line = make_batch_and_line(sku='BLACK_CHAIR', batch_qty=10, line_qty=5)
 
   assert batch.can_allocate(order_line)
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
-  order_line = OrderLine(sku='BLACK_CHAIR', order_id='123', quantity=10)
-  batch = Batch(ref=333, sku='BLACK_CHAIR', quantity=5)
+  batch, order_line = make_batch_and_line(sku='BLACK_CHAIR', batch_qty=5, line_qty=10)
 
   assert batch.can_allocate(order_line) == False
 
 
 def test_can_allocate_if_available_equal_to_required():
-  order_line = OrderLine(sku='BLACK_CHAIR', order_id='123', quantity=5)
-  batch = Batch(ref=333, sku='BLACK_CHAIR', quantity=5)
+  batch, order_line = make_batch_and_line(sku='BLACK_CHAIR', batch_qty=10, line_qty=10)
 
   assert batch.can_allocate(order_line)
+
+
+def test_can_allocate_if_avaliable_greater_than_required():
+  pass
