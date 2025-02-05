7f89d30be917619b5848670a623931cb650bcac1
Raiuri Santos <raiuresantos@ymail.com>
2025-02-05 01:07:07 -0300

    add more tests to batch class

--- src/models/batch.py
+++ src/models/batch.py
@@ -20,12 +20,28 @@ class OrderLine:
 
 
 class Batch:
+  batches = set()
+
   def __init__(self, ref: str, sku: str, quantity: int):
     self.ref = ref
     self.sku = sku
     self.avaliable_qty = quantity
+    self._allocations = set()
 
 
-  def allocate(self, order_line: OrderLine):
+  def can_allocate(self, order_line: OrderLine) -> bool:
+    if self.avaliable_qty < order_line.quantity:
+      return False
+    return True
+
+
+  def allocate(self, order_line: OrderLine) -> int:
+    self.can_allocate(order_line)
+    self._allocations.add(order_line)
     self.avaliable_qty -= order_line.quantity
 
+
+order_line = OrderLine(sku='BLACK_CHAIR', order_id='123', quantity=5)
+batch = Batch(ref=333, sku='BLACK_CHAIR', quantity=10)
+  
+print(batch.can_allocate(order_line))
--- src/models/test_batches.py
+++ src/models/test_batches.py
@@ -10,12 +10,31 @@
 
 from .batch import OrderLine, Batch
 
-def test_allocating_to_a_batch_reduces_the_avaliable_quantity():
-  order_line = OrderLine(sku='abc', order_id='123', quantity=3)
-  batch = Batch(ref=333, sku='abc', quantity=20)
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+  order_line = OrderLine(sku='BLACK_CHAIR', order_id='123', quantity=3)
+  batch = Batch(ref=333, sku='BLACK_CHAIR', quantity=20)
 
   batch.allocate(order_line)
 
   assert batch.avaliable_qty == 17
 
 
+def test_can_allocate_if_available_greater_than_required():
+  order_line = OrderLine(sku='BLACK_CHAIR', order_id='123', quantity=5)
+  batch = Batch(ref=333, sku='BLACK_CHAIR', quantity=10)
+
+  assert batch.can_allocate(order_line)
+
+
+def test_cannot_allocate_if_available_smaller_than_required():
+  order_line = OrderLine(sku='BLACK_CHAIR', order_id='123', quantity=10)
+  batch = Batch(ref=333, sku='BLACK_CHAIR', quantity=5)
+
+  assert batch.can_allocate(order_line) == False
+
+
+def test_can_allocate_if_available_equal_to_required():
+  order_line = OrderLine(sku='BLACK_CHAIR', order_id='123', quantity=5)
+  batch = Batch(ref=333, sku='BLACK_CHAIR', quantity=5)
+
+  assert batch.can_allocate(order_line)
